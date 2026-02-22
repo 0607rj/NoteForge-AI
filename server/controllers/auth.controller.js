dccbf23dd3c244b7ba4d6283e44a2733234621b2
happyhome18110@gmail.com <happyhome18110@gmail.com>
2026-02-22 13:18:52 +0530

    Fix cookie sameSite attribute for cross-origin authentication

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -16,7 +16,7 @@ export const googleAuth = async (req,res) => {
         res.cookie("token" , token , {
             httpOnly:true,
             secure:true,
-            samesite:"none",
+            sameSite:"none",
             maxAge:7 * 24 * 60 * 60 * 1000
 
         })
@@ -29,7 +29,11 @@ export const googleAuth = async (req,res) => {
 
 export const logOut = async (req,res) => {
     try {
-        await res.clearCookie("token")
+        res.clearCookie("token", {
+            httpOnly:true,
+            secure:true,
+            sameSite:"none"
+        })
          return res.status(200).json({message:"LogOut Successfully"})
     } catch (error) {
         return res.status(500).json({message:`Logout Error  ${error}`})
