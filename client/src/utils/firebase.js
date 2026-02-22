e205295834221227e0ae791187901645c528a834
happyhome18110@gmail.com <happyhome18110@gmail.com>
2026-02-22 12:48:28 +0530

    changes env of client

--- client/src/utils/firebase.js
+++ client/src/utils/firebase.js
@@ -4,12 +4,12 @@ import {getAuth, GoogleAuthProvider} from "firebase/auth"
 
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_APIKEY,
-  authDomain: "ai-notes-825c5.firebaseapp.com",
-  projectId: "ai-notes-825c5",
-  storageBucket: "ai-notes-825c5.firebasestorage.app",
-  messagingSenderId: "266673710198",
-  appId: "1:266673710198:web:acdab5844d2c53135b6b57",
-  measurementId: "G-NXGV6KNM4F"
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 };
 
 
